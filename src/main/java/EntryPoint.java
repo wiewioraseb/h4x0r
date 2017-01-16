530f464f9907555d2d0f8486fb1223969e65d735
Sebastian Wiewiora <swiewiora>
2017-01-16 22:47:59 +0100

    Palindrome

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,5 +1,22 @@
 public class EntryPoint {
     public static void main(String[] args) {
         System.out.println("Start");
+        EntryPoint ep = new EntryPoint();
+
+        System.out.println("Palindrome");
+        String text = "ikajaki";
+        boolean isPalindrome = ep.checkIfPalindromeClassic(text);
+        System.out.println("String " + text + "is palindrome: " + isPalindrome);
+
+
+    }
+
+    private boolean checkIfPalindromeClassic(String text) {
+        boolean result = false;
+        char[] chText = text.toCharArray();
+        for (int i = 0; i < chText.length / 2; i++) {
+            result = chText[i] == chText[chText.length-1 - i];
+        }
+        return result;
     }
 }
