b1ed4297d8ea68d8519770317786a5680698aeea
Sebastian Wiewiora <swiewiora>
2017-01-17 20:23:21 +0100

    fix fizz

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -32,15 +32,15 @@ public class EntryPoint {
     private void fizzBuzz() {
         for (int i = 1; i <= 100; i++) {
             System.out.print(i + " ");
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else
-            if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else
             if (i % 3 == 0) {
-                System.out.println("Fizz");
+                System.out.print("Fizz");
             }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+            System.out.println("");
         }
     }
+
+
 }
