295cb3ee4f437dba2da470788aa44e38e7447b40
Sebastian Wiewiora <swiewiora>
2017-01-22 21:55:20 +0100

    sum of even num with stream

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -2,6 +2,7 @@ import Sort.BubbleSort;
 import Sort.InsertionSort;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class EntryPoint {
     public static void main(String[] args) {
@@ -58,6 +59,24 @@ public class EntryPoint {
 
     private int factorial(int param) {
         return param > 1 ? param * factorial(param - 1) : param;
+    }
+
+    private int sumOfOddNumbers() {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+        int result = 0;
+
+/*
+        // Old fashioned way
+        for(int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) result += i;
+        }
+*/
+
+        result = Arrays.stream(arr)
+                .filter(x -> x % 2 == 0)
+                .sum();
+
+        return result;
     }
 
 }
